f4feebd48f3e624a5388ee21f46886db1531fb92
Haniali1 <96863467+Haniali1@users.noreply.github.com>
2026-08-20 04:25:33 +0300

    Update cardTheme to use CardThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MazzariApp extends StatelessWidget {
           brightness: Brightness.light,
         ),
         scaffoldBackgroundColor: const Color(0xFFF5F5F5),
-        cardTheme: CardTheme(
+        cardTheme: const CardThemeData(
           elevation: 2,
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
         ),
